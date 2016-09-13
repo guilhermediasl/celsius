9caab153a04a46f41c925e9b45b2af93a9462f41
Kewin Lima Da Silva <lima_kewin@hotmail.com>
2016-09-13 10:09:15 -0300

    Arrumando sensores e verificadores

--- utils/teste-sensores/teste-sensores.ino
+++ utils/teste-sensores/teste-sensores.ino
@@ -5,30 +5,50 @@
  */
 
 //Define a quantidade de sensores e tempo para legibilidade da saída serial
-#define NUMERO_DE_SENSORES 9
+#define NUMERO_DE_SENSORES 8
+#define NUMERO_DE_VERIFICADORES 2
 #define TEMPO 800
 
 //Define um vetor com todos os pinos dos sensores
-int sensores[NUMERO_DE_SENSORES] = {A0,A1,A2,A3,A4,A5,A6,A7,A8};
+int sensores[NUMERO_DE_SENSORES] = {A2,A3,A4,A5,A6,A7};
+int verificadores[NUMERO_DE_VERIFICADORES] = {A0,A1};
 
 //Define os pinos dos sensores como entradas e inicia um fluxo serial
 void setup() {
-	for(int i = 0; i < NUMERO_DE_SENSORES; i++){
-		pinMode(sensores[i], INPUT);
-	}
+		for(int i = 0; i < NUMERO_DE_SENSORES; i++)
+			pinMode(sensores[i], INPUT);
+		for(int n = 0; n < NUMERO_DE_VERIFICADORES; n++)
+			pinMode(verificadores[n]);
 
 	Serial.begin(9600);
  }
 
 //Lê todos os sensores e imprime no monitor serial
 void loop() {
+	Serial.print(" VERIFICAROES: ");
+	for(int n = 0; n<NUMERO_DE_VERIFICADORES; n++ ){
+		if(n == NUMERO_DE_VERIFICADORES-1)
+			Serial.println(analogRead(verificadores[n]));
+		else{
+			
+			Serial.print(analogRead(verificadores[n]));
+			Serial.print(" - ");
+			
+		}
+	}
+	Serial.print(" SENSORES: ");
+	for(int n = 0; n<NUMERO_DE_SENSORES; n++ ){
+		if(n == NUMERO_DE_SENSORES-1)
+			Serial.println(analogRead(sensores[n]));
+		else{
+			
+			Serial.print(analogRead(sensores[n]));
+			Serial.print(" - ");
+			
+		}
+	}
 	for(int i = 0; i < NUMERO_DE_SENSORES; i++) {
-		Serial.print("Sensor ");
-		Serial.print(i + 1);
-		Serial.print(": ");
 		Serial.println(analogRead(sensores[i]));
 	}
-	Serial.println("");
-	Serial.println("***********")
 	delay(TEMPO);	//Delay para legibilidade do código. 
 }
