6f7df13d7babfda30edaf4cc67bcb2339a6b696c
equipecarranca22 <carrancanear@gmail.com>
2016-11-12 15:22:22 -0300

    Testes sensores celsius 2.0
    Mudanças na organização dos sensores

---
+++ utils/celsius_2_0_teste.ino
@@ -0,0 +1,45 @@
+
+#define NUMERO_DE_SENSORES 7
+#define NUMERO_DE_VERIFICADORES 2
+#define TEMPO 800
+
+//Define um vetor com todos os pinos dos sensores
+int sensores[NUMERO_DE_SENSORES] = {A6,A5,A4,A3,A2,A1,A0};
+int verificadorEsquerdo = A7;
+int verificadorDireito = 4;
+//Define os pinos dos sensores como entradas e inicia um fluxo serial
+void setup() {
+		for(int i = 0; i < NUMERO_DE_SENSORES; i++)
+			pinMode(sensores[i], INPUT);
+		pinMode (verificadorEsquerdo, INPUT);
+                pinMode (verificadorDireito, INPUT);
+
+	Serial.begin(9600);
+ }
+
+//Lê todos os sensores e imprime no monitor serial
+void loop() {
+	Serial.print(" VERIFICAROES: ");
+	for(int n = 0; n<NUMERO_DE_VERIFICADORES; n++ ){
+		if(n == NUMERO_DE_VERIFICADORES-1)
+			Serial.println(analogRead(verificador));
+		else{
+			
+			Serial.print(analogRead(verificador));
+			Serial.print(" - ");
+			
+		}
+	}
+	Serial.print(" SENSORES: ");
+	for(int n = 0; n<NUMERO_DE_SENSORES; n++ ){
+		if(n == NUMERO_DE_SENSORES-1)
+			Serial.println(analogRead(sensores[n]));
+		else{
+			
+			Serial.print(analogRead(sensores[n]));
+			Serial.print(" - ");
+			
+		}
+	}
+	delay(TEMPO);	//Delay para legibilidade do código. 
+}
